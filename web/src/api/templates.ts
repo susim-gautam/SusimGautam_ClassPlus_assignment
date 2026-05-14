2bfbc27a72018f27091520497e470fcf55ad3c34
Susim Gautam <155796548+susim-gautam@users.noreply.github.com>
2026-05-14 14:18:24 +0530

    Delete web directory

--- web/src/api/templates.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './client'
-import type { GreetingTemplate } from '../types'
-
-export async function fetchTemplates(): Promise<GreetingTemplate[]> {
-  const out = await api<{ templates: GreetingTemplate[] }>('/api/templates', {
-    method: 'GET',
-  })
-  return out.templates
-}
